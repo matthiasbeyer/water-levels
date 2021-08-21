1c7b61c08ef0aff473ea6e03f7a16e716255935f
Matthias Beyer <mail@beyermatthias.de>
2021-08-21 11:42:07 +0200

    Remove impl for element simulation
    After refinement of the requirements, it is now clear that the levels
    are represented as floating point numbers.
    
    Thus, a landscape like:
    
        [ 2, 1, 1 ]
    
    with one hour of rain would end in
    
        [ 2.33, 2.33, 2.33 ]
    
    so the implementation must take care of shipping water around from the
    first element to the third in this landscape, for example.
    
    Thus, the implementation must be rewritten to be more "recursive"
    (although because we are implementing a simulation here, it can be
    iterative, actually).
    
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/backend/sim.rs
+++ src/backend/sim.rs
@@ -33,78 +33,7 @@ impl ElementRainingSimulation {
     pub async fn let_it_rain(self, mut hours: usize) -> Result<Self> {
         log::debug!("Let it rain for {} hours", hours);
 
-        while hours != 0 {
-            match (self.left_neighbor.as_ref(), self.right_neighbor.as_ref()) {
-                // simple case: this is the only element on the landscape, so it only rains on this
-                // element
-                (None, None) => {
-                    log::debug!("No neighbors, catching all the rain in myself");
-                    self.element.write().await.increase_rain_level(RainElement::FullRainElement)
-                },
-
-                // This element is the rightmost element in the landscape
-                (Some(left), None) => {
-                    log::debug!("Only a left neighbor");
-                    let (mut left_writelock, mut this_writelock) = tokio::join!(left.write(), self.element.write());
-                    log::trace!("left: {}, self: {}", left_writelock.get_current_height(), this_writelock.get_current_height());
-
-                    if left_writelock.get_current_height() < this_writelock.get_current_height() {
-                        log::debug!("Rain floats to my left");
-                        left_writelock.increase_rain_level(RainElement::FullRainElement);
-                    } else {
-                        log::debug!("Rain floats to me");
-                        this_writelock.increase_rain_level(RainElement::FullRainElement);
-                    }
-                }
-
-                // This element is the leftmost element in the landscape
-                (None, Some(right)) => {
-                    log::debug!("Only a right neighbor");
-                    let (mut this_writelock, mut right_writelock) = tokio::join!(self.element.write(), right.write());
-                    log::trace!("self: {}, right: {}", this_writelock.get_current_height(), right_writelock.get_current_height());
-
-                    if this_writelock.get_current_height() > right_writelock.get_current_height() {
-                        log::debug!("Rain floats to my right");
-                        right_writelock.increase_rain_level(RainElement::FullRainElement);
-                    } else {
-                        log::debug!("Rain floats to me");
-                        this_writelock.increase_rain_level(RainElement::FullRainElement);
-                    }
-                }
-
-                (Some(left), Some(right)) => {
-                    log::debug!("Both neighbors");
-                    let (mut left_writelock, mut this_writelock, mut right_writelock) = tokio::join!(left.write(), self.element.write(), right.write());
-
-                    let l_h = left_writelock.get_current_height();
-                    let t_h = this_writelock.get_current_height();
-                    let r_h = right_writelock.get_current_height();
-                    log::trace!("left: {}, self: {}, right: {}", l_h, t_h, r_h);
-
-                    if l_h < t_h && r_h < t_h {
-                        // we are higher than both our neighbors
-                        log::debug!("self is highest, floating to both neighbors");
-                        left_writelock.increase_rain_level(RainElement::HalfRainElement);
-                        right_writelock.increase_rain_level(RainElement::HalfRainElement);
-                    } else if l_h < t_h && r_h >= t_h {
-                        // only left of us is lower than us
-                        log::debug!("left is lower than me, floating to left");
-                        left_writelock.increase_rain_level(RainElement::FullRainElement);
-                    } else if l_h >= t_h && r_h < t_h {
-                        // only right of us is lower than us
-                        log::debug!("right is lower than me, floating to right");
-                        right_writelock.increase_rain_level(RainElement::FullRainElement);
-                    } else if l_h >= t_h && r_h >= t_h {
-                        // both neighbors are higher than us
-                        log::debug!("both are higher than me, floating to me");
-                        this_writelock.increase_rain_level(RainElement::FullRainElement);
-                    }
-                }
-            }
-
-            hours -= 1;
-            log::debug!("{} hours left", hours);
-        }
+        unimplemented!()
 
         Ok(self)
     }

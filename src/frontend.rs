3ad78b6e1c820ef86d98fb6c1f28902479693230
Matthias Beyer <mail@beyermatthias.de>
2021-08-29 13:44:13 +0200

    Fix deserializing of Landscape
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/frontend.rs
+++ src/frontend.rs
@@ -2,6 +2,9 @@ use actix_web::Result;
 use actix_web::get;
 use actix_web::post;
 use actix_web::web;
+use actix_web::HttpRequest;
+use actix_web::HttpResponse;
+use actix_web::Error;
 
 #[get("/")]
 pub async fn index() -> Result<maud::Markup> {
@@ -45,7 +48,7 @@ pub async fn make_landscape(form: web::Form<LandscapeSize>) -> Result<maud::Mark
                 h1 { "Hello World!" }
 
                 p { "Please fill in the landscape values" }
-                form action="/calculate" method="post" {
+                form action="/calculate" method="get" {
                     @for _n in 0..form.elements {
                         p {
                             input type="number" name="levels[]" checked;
@@ -68,27 +71,43 @@ pub struct Landscape {
     hours: usize,
 }
 
-#[post("/calculate")]
-pub async fn calculate(form: web::Form<Landscape>) -> Result<maud::Markup> {
-    let ls = crate::backend::landscape::Landscape::new(form.levels.clone()).rain(form.hours);
+
+// I don't know why actix-web wont work with a POST parameter here,
+// but taken from
+//
+//  https://github.com/actix/actix-web/issues/259#issuecomment-513942790
+//
+// this seems to work.
+//
+// This is neither how you do it, nor how I would like to do it, but it
+// (Insert some random swearwords here) works.
+//
+#[get("/calculate")]
+pub async fn calculate(req: HttpRequest) -> Result<maud::Markup> {
+    let config = serde_qs::Config::new(10, false);
+    let ls: Landscape = config.deserialize_str(&req.query_string())
+        .map_err(|e| actix_web::error::ErrorInternalServerError(format!("{}", e)))?;
+
+    let calculated_landscape = crate::backend::landscape::Landscape::new(ls.levels.clone()).rain(ls.hours);
     Ok(maud::html! {
         html {
             body {
                 h1 { "Landscape!" }
 
-                p { "Filling in " (form.hours) " hours" }
+                p { "Filling in " (ls.hours) " hours" }
 
-                @for value in &form.levels {
+                @for value in &ls.levels {
                     p { (value) }
                 }
 
                 h2 { "Filled" }
 
-                @for val in ls.into_inner() {
+                @for val in calculated_landscape.into_inner() {
                     p { (val.0) " + " (val.1) }
                 }
             }
         }
     })
 }
+
 

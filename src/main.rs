fbffca727dde4d27e2e5232d6e5ed2c351257698
Matthias Beyer <mail@beyermatthias.de>
2021-09-04 15:52:52 +0200

    Add passing bind addr and port via ENV
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
+use std::str::FromStr;
+
 use actix_web::App;
 use actix_web::HttpServer;
 use actix_web::middleware::Logger;
+use anyhow::Context;
 
 mod frontend;
 mod backend;
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> anyhow::Result<()> {
     if let Err(std::env::VarError::NotPresent) = std::env::var("RUST_LOG") {
         std::env::set_var("RUST_LOG", "actix_web=info");
     }
     env_logger::init();
+
+    let bind = match std::env::var("WATER_LEVELS_HOST") {
+        Ok(var) => var,
+        Err(e) => anyhow::bail!("WATER_LEVELS_HOST not available: {:?}", e),
+    };
+
+    let port = match std::env::var("WATER_LEVELS_PORT") {
+        Ok(var) => u16::from_str(&var).context("Parsing port to u16")?,
+        Err(e) => anyhow::bail!("WATER_LEVELS_PORT not available: {:?}", e),
+    };
 
     HttpServer::new(|| {
         App::new()
@@ -20,7 +33,8 @@ async fn main() -> std::io::Result<()> {
             .service(crate::frontend::make_landscape)
             .service(crate::frontend::calculate)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind((bind, port))?
     .run()
     .await
+    .map_err(anyhow::Error::from)
 }
